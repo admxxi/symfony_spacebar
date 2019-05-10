b102ec1f53abe2cf705973652d3156ce82adea74
Andre Monteiro <admxxi@gmail.com>
2019-05-10 18:10:28 +0100

    Installing Twig + Creating first template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ArticleController
+ * @package App\Controller
+ */
 class ArticleController extends AbstractController
 {
     /**
@@ -30,11 +34,21 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/news/{slug}")
+     * @param $slug
      * @return Response
      */
     public function show($slug)
     {
-        return new Response("Article => " . $slug);
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render("article/show.html.twig", [
+            'title' => ucwords(str_replace('-', '', $slug)),
+            'comments' => $comments
+        ]);
     }
 
 }
